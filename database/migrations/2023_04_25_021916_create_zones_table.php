f4b77b7178ea7d8096b4fe626ac3386494949f72
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-04-25 03:33:01 +0100

    Database Schema

---
+++ database/migrations/2023_04_25_021916_create_zones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Zone;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('zones', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->integer('price_per_hour');
+
+            $table->timestamps();
+        });
+
+        
+        Zone::create(['name' => 'Green Zone', 'price_per_hour' => 100]);
+        Zone::create(['name' => 'Yellow Zone', 'price_per_hour' => 200]);
+        Zone::create(['name' => 'Red Zone', 'price_per_hour' => 300]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('zones');
+    }
+};
