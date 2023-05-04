dad6a9544f0e6acc4cf97383f45ac9947f172377
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-05-04 15:32:33 +0100

    Vechicle a null value

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -27,10 +27,11 @@ class ParkingResource extends JsonResource
                 'name' => $this->zone->name,
                 'price_per_hour' => $this->zone->price_per_hour,
             ],
-            'vehicle' => [
+            'vehicle' => $this->vehicle ? [
                 'plate_number' => $this->vehicle->plate_number,
                 'description'  => $this->vehicle->description,
-            ],
+            ] : null,
+            // made the vehicle null
             'start_time' => $this->start_time->toDateTimeString(),
             'stop_time' => $this->stop_time?->toDateTimeString(),
             'total_price' => $totalPrice,
